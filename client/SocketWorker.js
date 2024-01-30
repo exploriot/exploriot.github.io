8743ebd216439dade38cfc2d45717c13c8f59589
Oğuzhan <oguzhan.umutlu123@gmail.com>
2024-01-30 23:15:28 +0300

    bug fixes

--- client/SocketWorker.js
+++ client/SocketWorker.js
@@ -1,8 +1,11 @@
-onmessage = ({data: init}) => {
-    init = JSON.parse(init);
-    let hasTriedSecure = false;
-    let connected = false;
-    let socket = new WebSocket("ws" + init.url);
+let hasEverConnected = false;
+let hasTriedSecure = false;
+let connected = false;
+let socket;
+let init;
+
+function makeSocket(secure = true) {
+    socket = new WebSocket("ws" + (secure ? "s" : "") + init.url);
     socket.addEventListener("message", e => {
         const pk = JSON.parse(e.data);
         if (pk.type === init.SERVER_PING) {
@@ -12,18 +15,25 @@ onmessage = ({data: init}) => {
         postMessage("0" + e.data);
     });
     socket.addEventListener("error", () => {
-        if (!connected || !hasTriedSecure) {
-            socket = new WebSocket("wss" + init.url);
+        if (!hasEverConnected && !hasTriedSecure) {
+            hasTriedSecure = true;
+            makeSocket(false);
         }
     });
     socket.addEventListener("open", () => {
+        hasEverConnected = true;
         connected = true;
         postMessage("1");
     });
     socket.addEventListener("close", () => {
         connected = false;
-        postMessage("2");
+        if (hasEverConnected) postMessage("2");
     });
+}
+
+onmessage = ({data}) => {
+    init = JSON.parse(data);
+    makeSocket(true);
     onmessage = ({data}) => {
         if (data === "!") return socket.close();
         socket.send(data);
